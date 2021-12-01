2233332408f778ebf6304b256bbb5689faa4cdc9
HoudaGasmi <houdagasmi70@gmail.com>
2021-12-01 22:27:13 +0100

    depot

--- src/app/hotel.service.ts
+++ src/app/hotel.service.ts
@@ -12,22 +12,22 @@ export class HotelService {
   lesHotels:Hotel[]=[
     new Hotel("Royal Kenz Thalasso & Spa",102,
     [
-      new Picture("/assets/thalasso.jpg","/assets/sousse.jpg","/assets/kenz.jpg","/assets/thalasso1.jpg","/assets/thalasso2.jpg")],"Sousse - Tunisie","/assets/5etoile.png",true),
+      new Picture("/assets/thalasso.jpg","/assets/sousse.jpg","/assets/kenz.jpg","/assets/thalasso1.jpg","/assets/thalasso2.jpg")],"Sousse","/assets/5etoile.png",true),
     new Hotel("Steigenberger Marhaba Thalasso",150,
     [
-      new Picture("/assets/hammamet3.jpg","/assets/hammamet1.jpg","/assets/hammamet1.jpg")],"Hammamet - Tunisie","/assets/4etoile.png",true),
+      new Picture("/assets/hammamet3.jpg","/assets/hammamet1.jpg","/assets/hammamet.jpg","/assets/hammamet4.jpg","/assets/hammamet2.jpg")],"Hammamet","/assets/4etoile.png",true),
     new Hotel("Blue Marine Hotel & Thalasso",92,
     [
-      new Picture("/assets/Blue.jpg","/assets/Blue1.jpg","/assets/Blue2.jpg")],"Hammamet - Tunisie","assets/4etoile.png",false),
+      new Picture("/assets/Blue.jpg","/assets/Blue1.jpg","/assets/Blue2.jpg","/assets/Blue3.jpg","/assets/Blue4.jpg")],"Hammamet","assets/4etoile.png",false),
       new Hotel('Hotel La Cigale',140,
-      [new Picture ("/assets/cigal1.jpg","/assets/cigal4.jpg","/assets/cigal2.jpg","/assets/cigal3.jpg","/assets/cigal.jpg")],'Tabarka - Tunisie','/assets/5etoile.png',false),
+      [new Picture ("/assets/cigal1.jpg","/assets/cigal4.jpg","/assets/cigal2.jpg","/assets/cigal3.jpg","/assets/cigal.jpg")],'Tabarka','/assets/5etoile.png',false),
     new Hotel('Hotel Thabraca',99,
-    [new Picture ("/assets/tabraka1.jpg","/assets/tabraka2.jpg","/assets/tabraka3.jpg","/assets/tabraka4.jpg","/assets/tabraka.jpg")],'Tabarka - Tunisie','/assets/4etoile.png',true),
+    [new Picture ("/assets/tabraka1.jpg","/assets/tabraka2.jpg","/assets/tabraka3.jpg","/assets/tabraka4.jpg","/assets/tabraka.jpg")],'Tabarka','/assets/4etoile.png',true),
     new Hotel('Hotel Mövenpick ',225,
-    [new Picture ("/assets/mvmpik.jpg","/assets/mvp.jpg","/assets/mvp1.jpg","/assets/mvp2.jpg","/assets/mvp3.jpg")],'Sousse - Tunisie','/assets/5etoile.png',false),
+    [new Picture ("/assets/mvmpik.jpg","/assets/mvp.jpg","/assets/mvp1.jpg","/assets/mvp2.jpg","/assets/mvp3.jpg")],'Sousse','/assets/5etoile.png',false),
     new Hotel("Royal Thalassa Monastir",99,
     [
-      new Picture("/assets/monastir.jpg","/assets/monastir1.jpg","/assets/monastir2.jpg","/assets/monastir3.jpg","/assets/monastir4.jpg")],"Monastir - Tunisie","/assets/4etoile.png",true)
+      new Picture("/assets/monastir.jpg","/assets/monastir1.jpg","/assets/monastir2.jpg","/assets/monastir3.jpg","/assets/monastir4.jpg")],"Monastir","/assets/4etoile.png",true)
     
   ]
    login:Login[]=[
