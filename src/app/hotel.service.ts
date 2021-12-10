cf919ef3607f5aa7885a6e6c6ff4a7c3674f35a5
HoudaGasmi <houdagasmi70@gmail.com>
2021-12-10 22:34:37 +0100

    depot

--- src/app/hotel.service.ts
+++ src/app/hotel.service.ts
@@ -13,29 +13,37 @@ export class HotelService {
     new Hotel("a","Royal Kenz Thalasso & Spa",102,
     [
       new Picture("/assets/thalasso.jpg","/assets/sousse.jpg","/assets/kenz.jpg","/assets/thalasso1.jpg","/assets/thalasso2.jpg")],"Sousse","/assets/5etoile.png",true),
-    new Hotel("b",'Hotel Mövenpick ',225,
+    
+      new Hotel("b",'Hotel Mövenpick ',225,
       [new Picture ("/assets/mvmpik.jpg","/assets/mvp.jpg","/assets/mvp1.jpg","/assets/mvp2.jpg","/assets/mvp3.jpg")],'Sousse','/assets/5etoile.png',false),
+      
       new Hotel("c","Steigenberger Marhaba Thalasso",150,
     [
       new Picture("/assets/hammamet3.jpg","/assets/hammamet1.jpg","/assets/hammamet.jpg","/assets/hammamet4.jpg","/assets/hammamet2.jpg")],"Hammamet","/assets/4etoile.png",true),
-    new Hotel("d","Blue Marine Hotel & Thalasso",92,
+    
+      new Hotel("d","Blue Marine Hotel & Thalasso",92,
     [
       new Picture("/assets/Blue.jpg","/assets/Blue1.jpg","/assets/Blue2.jpg","/assets/Blue3.jpg","/assets/Blue4.jpg")],"Hammamet","assets/4etoile.png",false),
-    new Hotel("e",'Hotel La Cigale',140,
+    
+      new Hotel("e",'Hotel La Cigale',140,
       [new Picture ("/assets/cigal1.jpg","/assets/cigal4.jpg","/assets/cigal2.jpg","/assets/cigal3.jpg","/assets/cigal.jpg")],'Tabarka','/assets/5etoile.png',false),
-    new Hotel("f",'Hotel Thabraca',99,
+    
+      new Hotel("f",'Hotel Thabraca',99,
     [new Picture ("/assets/tabraka1.jpg","/assets/tabraka2.jpg","/assets/tabraka3.jpg","/assets/tabraka4.jpg","/assets/tabraka.jpg")],'Tabarka','/assets/4etoile.png',true),
     
     new Hotel("g","Iberostar Selection Kuriat Palace ",174,
     [
       new Picture("/assets/monastir5.jpg","/assets/monastir6.jpg","/assets/monastir7.jpg","/assets/monastir8.jpg","/assets/monastir9.jpg")],"Monastir","/assets/5etoile.png",true),
-    new Hotel("h","Royal Thalassa Monastir",153,
+    
+      new Hotel("h","Royal Thalassa Monastir",153,
     [
       new Picture("/assets/monastir.jpg","/assets/monastir1.jpg","/assets/monastir2.jpg","/assets/monastir3.jpg","/assets/monastir4.jpg")],"Monastir","/assets/4etoile.png",true),
-    new Hotel("i","Iberostar Royal El Mansour",156,
+    
+      new Hotel("i","Iberostar Royal El Mansour",156,
       [
         new Picture("/assets/mahdia.jpg","/assets/mahdia1.jpg","/assets/mahdia2.jpg","/assets/mahdia3.jpg","/assets/mahdia4.jpg")],"Mahdia","/assets/5etoile.png",true),
-    new Hotel("j","Mahdia Palace ",138,
+    
+        new Hotel("j","Mahdia Palace ",138,
       [
         new Picture("/assets/mahdia5.jpg","/assets/mahdia6.jpg","/assets/mahdia7.jpg","/assets/mahdia8.jpg","/assets/mahdia9.jpg")],"Mahdia","/assets/4etoile.png",false)
   ]
